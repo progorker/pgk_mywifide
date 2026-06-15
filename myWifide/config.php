93f943283b10571fc89784336d99aafeee1adfc2
[ progorker ] Zino's Petrel <zinospetrel@sdf.org>
2026-06-15 07:36:00 +0700

    Add files via upload

--- myWifide/config.php
+++ myWifide/config.php
@@ -1,11 +1,33 @@
 <?php
 /*
+ * =====] Following functions has license as [=====
+ *
+ * + check_http_headers_for_mobile()
+ * + g_match(string $regex, string $userAgent)
+ * + match_user_agent_with_first_found_matching_rule( $userAgent )
+ *
+ * -----
+ *
+ * Copyright (c) 2021 Şerban Ghiţă, Nick Ilyin and contributors.
+ *
+ * + License: MIT
+ * 
+ * + License URL: https://github.com/serbanghita/Mobile-Detect/blob/4.x/LICENSE
+ *
+ * + Source URL: https://github.com/serbanghita/Mobile-Detect
+ *
+ * =============================================
+ *
+ * =====] Other functions has license as  [=====
+ *
  * Copyright (c) 2026 Dinh Thoai Tran <zinospetrel@sdf.org>
  * All rights reserved.
  *
  * + Source URL: https://github.com/progorker/pgk_mywifide/
  *
  * + License: GPL-2.0
+ *
+ * =============================================
  */
 
 global $g_config;
